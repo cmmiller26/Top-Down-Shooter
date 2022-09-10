e719afd9782ae8cff81a5f4b0765ebc3608cf474
SnarlyZoo <colinmiller26@gmail.com>
2022-09-10 15:48:32 -0500

    Added Player Module

---
+++ src/StarterPlayer/StarterPlayerScripts/PlayerModule.lua
@@ -0,0 +1,24 @@
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local TDSController = require(ReplicatedStorage.Controllers.TDSController)
+
+local PlayerModule = {}
+PlayerModule.__index = PlayerModule
+
+function PlayerModule.new()
+    local self = {
+        player = Players.LocalPlayer,
+        camera = workspace.CurrentCamera,
+
+        controller = nil
+    }
+
+    setmetatable(self, PlayerModule)
+
+    self.controller = TDSController.new(self.player, self.camera)
+
+    return self
+end
+
+return PlayerModule.new()
