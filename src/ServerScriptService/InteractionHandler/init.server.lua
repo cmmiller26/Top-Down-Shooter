50aba09c8d492457b57745d4b25b3a11a001dcad
SnarlyZoo <colinmiller26@gmail.com>
2022-12-20 20:35:14 -0600

    Init rework

--- src/ServerScriptService/InteractionHandler/init.server.lua
+++
@@ -1,9 +0,0 @@
-local CollectionService = game:GetService("CollectionService")
-
-local Door = require(script.Door)
-
-for _, interaction in ipairs(CollectionService:GetTagged("Interaction")) do
-    if interaction.Type.Value == "Door" then
-        Door.new(interaction)
-    end
-end
--- src/ServerScriptService/RemoteHandler/init.server.lua
+++
@@ -1,6 +0,0 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
-local TDSPlayer = require(script.TDSPlayer)
-ReplicatedStorage.Controllers.TDSController.Remotes.Connect.OnServerEvent:Connect(function(player)
-    TDSPlayer.new(player)
-end)
